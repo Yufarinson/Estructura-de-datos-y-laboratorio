6abd23f6039d1e4821b30a2787c14796ecee0418
Yufarinson <yufarinxithodiaz@gmail.com>
2025-02-18 21:49:16 -0500

    Nuevo commit

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿using POOConcepts;
+
+
+try
+{
+    var date1 = new Date();
+    var date2 = new Date(1974, 12, 3);
+    var date3 = new Date(2020, 2, 29);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
